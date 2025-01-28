9af085c288e940a4701a2053b9101bdce2593474
David <atembarry12@gmail.com>
2025-01-28 22:44:04 +0100

    Dynamic Metadata Added on /stock/ticker

--- app/stock/[ticker]/page.tsx
+++ app/stock/[ticker]/page.tsx
@@ -1,5 +1,16 @@
 import APIRender from "./apiCalls"
 
+export async function generateMetadata({params} : {
+    params: Promise<{ticker: string}>
+}){
+    const ticker = (await params).ticker
+    return(
+        {
+            title: `${ticker.toUpperCase()} Stock Information`
+        }
+    )
+}
+
 export default async function RouteShow({
     params,
 }: {
