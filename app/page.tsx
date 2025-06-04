f01328a2e86816e58eb40cfccedd450292c3d790
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-04 14:04:43 +0100

    Add BodySection component to enhance homepage content and structure

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import stock_shot from "../public/stock.jpg";
 import { FaInstagram, FaMailBulk } from "react-icons/fa";
 import { FaGithub, FaLinkedin, FaXTwitter } from "react-icons/fa6";
 import Hero from "./components/home/hero";
+import BodySection from "./components/home/body";
 export const metadata: Metadata = {
   title: "Baxter: Guided Investments for a Better Today",
 };
@@ -14,90 +15,56 @@ export default function Home() {
     <div className="grid w-full">
       <HomeNav />
       <Hero />
-      {/* <BodySection />
-      <Offerings />
+      <BodySection />
+      {/* <Offerings />
       <CTA_Footer /> */}
     </div>
   );
 }
 
-// function Hero() {
+// function BodySection() {
 //   return (
-//     <section id="hero" className="p-2 my-2 md:p-4 md:my-2">
-//       <div className="grid text-center justify-items-center">
-//         <p className="md:text-[10rem] text-3xl md:text-6xl my-6">
-//           Never Make a Wrong Investment Again
-//         </p>
+//     <section className="p-2 my-2 md:p-4 md:my-2" id="body">
+//       <div className="grid">
+//         <p className="md:text-4xl text-2xl">Why Choose Baxter?</p>
+//         <div className="grid">
+//           <div>
+//             <p className="head_p">Intuitive Stock Charts</p>
 
-//         <div className="grid md:grid-cols-2 items-center justify-items-center">
-//           <p className="text-2xl my-4 text-justify indent-4 blurb">
-//             Baxter is your ultimate tool for making informed investment
-//             decisions. We provide up-to-date stock charts, detailed data, and
-//             essential guidance to help you navigate the complexities of the
-//             stock market with confidence and security. Our goal is to empower
-//             individuals, businesses and corporate bodies to make informed
-//             decisions and secure their assets over the short and long term.{" "}
-//           </p>
-//           <div className="grid justify-items-center">
-//             <Image
-//               src={stock_shot}
-//               alt="Stock Shot"
-//               className="rounded-xl w-[75%]"
-//             />
+//             <p className="side_p">
+//               Visualize stock performance with dynamic, interactive charts that
+//               are easy to customize and understand. Spot trends, compare stocks,
+//               and track historical data all in one place.{" "}
+//             </p>
+
+//             <p className="head_p">Comprehensive Market Data</p>
+
+//             <p className="side_p">
+//               Stay ahead of the market with up-to-the-minute data, including
+//               stock prices, volume, earnings, and more. Baxter equips you with
+//               all the information you need to seize opportunities.{" "}
+//             </p>
+
+//             <p className="head_p">Guided Investments</p>
+
+//             <p className="side_p">
+//               Not sure where to start? Our guided investment tools offer
+//               personalized insights and curated stock recommendations based on
+//               your goals and risk tolerance.,
+//             </p>
+
+//             <p className="head_p">Expert Resources</p>
+
+//             <p className="side_p">
+//               Access a wealth of knowledge through our tutorials, webinars, and
+//               articles created by seasoned investors and market analysts.{" "}
+//             </p>
 //           </div>
 //         </div>
-
-//         <span className="justify-self-center bg-pink-700 md:w-2/12 my-4 w-9/12 rounded-[1.75rem] text-center text-2xl hover:font-bold hover:bg-pink-600 transition-all md:hover:w-4/12 p-2">
-//           Read More...
-//         </span>
 //       </div>
 //     </section>
 //   );
 // }
-
-function BodySection() {
-  return (
-    <section className="p-2 my-2 md:p-4 md:my-2" id="body">
-      <div className="grid">
-        <p className="md:text-4xl text-2xl">Why Choose Baxter?</p>
-        <div className="grid">
-          <div>
-            <p className="head_p">Intuitive Stock Charts</p>
-
-            <p className="side_p">
-              Visualize stock performance with dynamic, interactive charts that
-              are easy to customize and understand. Spot trends, compare stocks,
-              and track historical data all in one place.{" "}
-            </p>
-
-            <p className="head_p">Comprehensive Market Data</p>
-
-            <p className="side_p">
-              Stay ahead of the market with up-to-the-minute data, including
-              stock prices, volume, earnings, and more. Baxter equips you with
-              all the information you need to seize opportunities.{" "}
-            </p>
-
-            <p className="head_p">Guided Investments</p>
-
-            <p className="side_p">
-              Not sure where to start? Our guided investment tools offer
-              personalized insights and curated stock recommendations based on
-              your goals and risk tolerance.,
-            </p>
-
-            <p className="head_p">Expert Resources</p>
-
-            <p className="side_p">
-              Access a wealth of knowledge through our tutorials, webinars, and
-              articles created by seasoned investors and market analysts.{" "}
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
 
 function Offerings() {
   return (
