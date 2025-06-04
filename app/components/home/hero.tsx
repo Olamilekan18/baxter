f745d33b21974d129bda70a8c1d2204f1ce744e1
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-04 13:35:27 +0100

    Update Hero component with new stock image and revised content for clarity

--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import stock_shot from "@/public/stock.jpg";
+import stock_shot from "@/public/stock-image.jpg";
 export default function Hero() {
   return (
     <section className="relative  text-white overflow-hidden pt-10">
@@ -16,16 +16,15 @@ export default function Hero() {
         <div className="flex flex-col md:flex-row items-center justify-around">
           <div className="w-full md:w-1/2 mb-12 md:mb-0 relative">
             <h1 className="text-5xl md:text-8xl font-bold mb-6 leading-tight ">
-              Leads
-              <br />
               <span className="bg-gradient-to-r from-blue-700 via-green-400 to-indigo-400 inline-block text-transparent bg-clip-text">
-                NexTech
+                Baxter
               </span>
             </h1>
 
             <p className="text-xl mb-5 text-gray-300 ">
-              Harnessing Research for developing Sustainable, Scalable, &
-              Impactful Solutions.
+              Our goal is to empower individuals, businesses and corporate
+              bodies to make informed decisions and secure their assets over the
+              short and long term.
             </p>
             <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 ">
               <button className="group relative w-full sm:w-auto px-6 py-3 min-w-[160px]">
@@ -70,7 +69,7 @@ export default function Hero() {
                       d="M13 10V3L4 14h7v7l9-11h-7z"
                     ></path>
                   </svg>
-                  <span>Data-Driven EarthTech Solutions</span>
+                  <span>Intuitive Stock Charts</span>
                 </li>
                 <li className="flex items-center">
                   <svg
@@ -87,7 +86,7 @@ export default function Hero() {
                       d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
                     ></path>
                   </svg>
-                  <span>Human-Centric AI & Analytics</span>
+                  <span>Comprehensive Market Data</span>
                 </li>
                 <li className="flex items-center">
                   <svg
@@ -104,7 +103,7 @@ export default function Hero() {
                       d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
                     ></path>
                   </svg>
-                  <span>NextGen Technology for a Sustainable Future</span>
+                  <span>Guided Investments and expert resources.</span>
                 </li>
               </ul>
             </div>
