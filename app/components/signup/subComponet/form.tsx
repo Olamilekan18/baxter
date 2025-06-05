44d6d75cac6da091a10f11370d13584755b0e028
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-05 22:26:55 +0100

    Refactor SignUpForm to remove commented-out password input code and streamline the submit button implementation

--- app/components/signup/subComponet/form.tsx
+++ app/components/signup/subComponet/form.tsx
@@ -18,38 +18,12 @@ export default function SignUpForm() {
         setPassword={setPassword}
         setShowPassword={setShowPassword}
       />
-      {/* <div>
-        <div className="flex justify-between items-center mb-1">
-          <label className="text-sm font-medium">Password</label>
-        </div>
-        <div className="flex items-center bg-[#232825] rounded-lg px-3 py-2 focus-within:ring-2 ring-green-400 transition">
-          <FiLock className="text-gray-400 mr-2" />
-          <input
-            type={showPassword ? "text" : "password"}
-            placeholder="Enter your password"
-            className="bg-transparent outline-none w-full text-white placeholder-gray-500"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="current-password"
-          />
-          <button
-            type="button"
-            onClick={() => setShowPassword((showPassword) => !showPassword)}
-            className="ml-2 text-gray-400 hover:text-green-400 focus:outline-none"
-            tabIndex={-1}
-            aria-label={showPassword ? "Hide password" : "Show password"}
-          >
-            {showPassword ? <FiEyeOff /> : <FiEye />}
-          </button>
-        </div>
-      </div> */}
 
-      <button
+      <input
+        value="Sign up"
         type="submit"
         className="w-full bg-[#40ff47] text-black font-semibold py-2 rounded-full hover:bg-[#32e93a] transition"
-      >
-        Sign up
-      </button>
+      />
     </form>
   );
 }
