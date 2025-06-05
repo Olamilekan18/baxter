3358439bb2ea27d6aeb27505500c3f54e089192e
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-05 01:08:44 +0100

    Add StockHomeNav component for improved navigation in stock pages; refactor Page component to include StockHomeNav

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,16 +1,15 @@
-import NewsItem from "../components/NewsItem"
-import { news_general } from "../stock/[ticker]/apiLoaders"
-import { newsItem } from "../type_definitions"
+import NewsItem from "../components/NewsItem";
+import { news_general } from "../stock/[ticker]/apiLoaders";
+import { newsItem } from "../type_definitions";
 
-export default async function Page(){
-    const news: newsItem[] = await news_general()
-
-    return(
-        <>
-        <h3 className="text-3xl">Market News and Updates</h3>
-        {
-            news.slice(0, 12).map((article) => <NewsItem object={article} key={article.id}/>)
-        }
-        </>
-    )
+export default async function Page() {
+  const news: newsItem[] = await news_general();
+  return (
+    <>
+      <h3 className="text-3xl">Market News and Updates</h3>
+      {news.slice(0, 12).map((article) => (
+        <NewsItem object={article} key={article.id} />
+      ))}
+    </>
+  );
 }
