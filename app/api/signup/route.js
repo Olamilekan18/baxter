a5abbd9635f4be315a2d7e821bf5ebd635b7e51a
samkiel488 <samuelezekiel@gmail.com>
2025-06-12 07:38:33 +0100

    Successfully integrated mongodb as the backend to save users info, the signup page now works fine.

--- app/api/signup/route.js
+++ app/api/signup/route.js
@@ -1,18 +1,20 @@
+import { connectMongoDB } from "app/lib/mongodb";
+import User from "app/models/user";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 export async function POST(req) {
-    try {
-      const { username, email, password } = await req.json();
+  try {
+    const { username, email, password } = await req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connectMongoDB();
+    await User.create({ username, email, password: hashedPassword });
 
-      console.log("Name: ", username);
-      console.log("Email: ", email);
-      console.log("Password: ", password);
-  
-      return NextResponse.json({ message: "User registered." }, { status: 201 });
-    } catch (error) {
-      return NextResponse.json(
-        { message: "An error occurred while registering the user." },
-        { status: 500 }
-      );
-    }
+    return NextResponse.json({ message: "User registered." }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occurred while registering the user." },
+      { status: 500 }
+    );
   }
+}
