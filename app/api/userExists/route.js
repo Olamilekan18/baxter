ca0e66c15d196500322912529e82f936ff0f39a0
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-13 23:03:58 +0100

    feat: update user registration and existence check to include username

--- app/api/userExists/route.js
+++ app/api/userExists/route.js
@@ -5,9 +5,14 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
   try {
     await connectMongoDB();
-    const { email } = await req.json();
+
+    var { email, username } = await req.json();
+    email = email.toLowerCase();
+    username = username.toLowerCase();
     const user = await User.findOne({ email }).select("_id");
+    const userN = await User.findOne({ username }).select("_id");
     console.log("user: ", user);
+    console.log("User: ", userN);
     return NextResponse.json({ user });
   } catch (error) {
     console.log(error);
